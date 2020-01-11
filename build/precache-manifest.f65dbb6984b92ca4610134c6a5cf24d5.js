73ec3558ee2b93ec4adeee3a597a01d4f8919354
BengBu-YueZhang <zhangyue_coder@163.com>
2020-01-11 22:40:38 +0800

    打包

---
+++ build/precache-manifest.f65dbb6984b92ca4610134c6a5cf24d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31958f1ae96fdf08db7eaeb9070d108f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a36189a831a617e5fa1",
+    "url": "/static/css/main.0f71d73b.chunk.css"
+  },
+  {
+    "revision": "94ac02e373b2b73fa6bb",
+    "url": "/static/js/2.4c3b8352.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.4c3b8352.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a36189a831a617e5fa1",
+    "url": "/static/js/main.d554c9c7.chunk.js"
+  },
+  {
+    "revision": "dbd973a323dff12a12a5",
+    "url": "/static/js/runtime-main.e550f8ce.js"
+  }
+]);
